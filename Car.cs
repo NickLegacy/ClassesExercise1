43a067e4a220cf50c0c68ad50a14aa4e31e544a6
NickLegacy <nicklegacy13@gmail.com>
2022-09-18 20:57:48 -0400

    initial commit

---
+++ Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+    }
+}
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var myCar = new Car();
+           myCar.Make = "Mazda";
+           myCar.Model = "Protege";
+           myCar.Year = 2010;
+
+            var toyota = new Car()
+            {
+                Make = "Toyota",
+                Model = "Camry",
+                Year = 2000,
+            };
+            var dodge = new Car()
+            {
+                Make = "Dodge",
+                Model = "Charger",
+                Year = 2005,
+            };
+       
+            var carList = new List<Car>() { myCar, toyota, dodge };
+            Console.WriteLine(carList.Count);
+            
+            foreach (var car in carList)
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+        }
+    }
+}
